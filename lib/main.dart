2d990e483553ea2cabaf0af71e80e3dbd2aafcc1
unknown <mohamednosair576@gmail.com>
2024-10-23 04:58:20 -0700

    loginw

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
+import 'package:flutter/material.dart';
 import 'package:flutter_advance/core/di/dependency_injection.dart';
 import 'package:flutter_advance/core/routing/app_router.dart';
 import 'package:flutter_advance/doc_app.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() async {
   setupGitIt();
-await ScreenUtil.ensureInitialized(); 
+   WidgetsFlutterBinding.ensureInitialized();
+  await ScreenUtil.ensureScreenSize();
   runApp(
     DocApp(
       appRouter: AppRouter(),
