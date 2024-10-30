f0d8e9affd16bc2f7311d8147ac1433779cb4568
unknown <mohamednosair576@gmail.com>
2024-10-30 03:25:09 -0700

    flavors

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -3,7 +3,7 @@ import 'package:flutter_advance/core/di/dependency_injection.dart';
 import 'package:flutter_advance/core/routing/app_router.dart';
 import 'package:flutter_advance/doc_app.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-
+//! to build flutter apk run (flutter build apk --flavor prod -target lib/main_prod.dart )
 void main() async {
   setupGitIt();
    WidgetsFlutterBinding.ensureInitialized();
